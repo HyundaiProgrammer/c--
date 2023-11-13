bdd5147dda419bf8c77ce782fab29f16f43198e6
HyundaiProgrammer <dw_426@naver.com>
2023-11-13 21:22:37 +0900

    hlklhklh

--- BaekJoon/25304.c
+++ BaekJoon/25304.c
@@ -5,11 +5,20 @@ int main () {
     int x;
     int n;
     int a,b;
+    int sum = 0;
     scanf("%d", &x);
     scanf("%d", &n);
 
-    for (int i = 0 ; i <n; i++){
-        printf("")
+    for (int i = 0 ; i < n; i++){
+        scanf("%d %d", &a, &b);
+        sum += a * b;
     }
+    if (sum == x){
+        printf("Yes\n");
+    }
+    else{
+        printf("No\n");
+    }
+    
     return 0;
 }
