cdbaf25176dd940192286235ef8ec77f297b0b06
HyundaiProgrammer <dw_426@naver.com>
2023-11-13 20:10:10 +0900

    inital commit

---
+++ BaekJoon/25304.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main () {
+    int x;
+    int n;
+    int a,b;
+    scanf("%d", &x);
+    scanf("%d", &n);
+
+    for (int i = 0 ; i <n; i++){
+        printf("")
+    }
+    return 0;
+}
